f216101501552410642ab007e25523b9341bb5ed
Elizabeth Gilbert <evgilber@andrew.cmu.edu>
2024-10-12 14:21:00 -0400

    ch24

--- object.h
+++ object.h
@@ -2,17 +2,25 @@
 #define clox_object_h
 
 #include "common.h"
+#include "chunk.h"
 #include "value.h"
 
 #define OBJ_TYPE(value)        (AS_OBJ(value)->type)
 
+#define IS_FUNCTION(value)     isObjType(value, OBJ_FUNCTION)
+#define IS_NATIVE(value)       isObjType(value, OBJ_NATIVE)
 #define IS_STRING(value)       isObjType(value, OBJ_STRING)
 
+#define AS_FUNCTION(value)     ((ObjFunction*)AS_OBJ(value))
+#define AS_NATIVE(value) \
+    (((ObjNative*)AS_OBJ(value))->function)
 #define AS_STRING(value)       ((ObjString*)AS_OBJ(value))
 
 #define AS_CSTRING(value)      (((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum {
+    OBJ_FUNCTION,
+    OBJ_NATIVE,
     OBJ_STRING,
 } ObjType;
 
@@ -22,6 +30,23 @@ struct Obj {
     // GC: pointer to the next Obj in the chain
     struct Obj* next;
 };
+typedef struct {
+    // Functions are first class in Lox, so they need to be actual Lox objects
+    // Thus ObjFunction has the same Obj header that all object types share.
+    Obj obj;
+    // the number of parameters the function expects
+    int arity;
+    Chunk chunk;
+    // the function’s name
+    ObjString* name;
+} ObjFunction;
+
+typedef Value (*NativeFn)(int argCount, Value* args);
+typedef struct {
+    Obj obj;
+    // pointer to the C function that implements the native behavior
+    NativeFn function;
+} ObjNative;
 
 struct ObjString {
     Obj obj;
@@ -31,6 +56,8 @@ struct ObjString {
     uint32_t hash;
 };
 
+ObjFunction* newFunction();
+ObjNative* newNative(NativeFn function);
 ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char* chars, int length);
 void printObject(Value value);
--- vm.c
+++ vm.c
@@ -1,6 +1,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #include "common.h"
 #include "compiler.h"
@@ -23,8 +24,12 @@
 // But when keeping things small for a book...
 VM vm;
 
+static Value clockNative(int argCount, Value* args) {
+    return NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
+}
 static void resetStack() {
     vm.stackTop = vm.stack;
+    vm.frameCount = 0;
 }
 static void runtimeError(const char* format, ...) {
     va_list args;
@@ -33,10 +38,38 @@ static void runtimeError(const char* format, ...) {
     va_end(args);
     fputs("\n", stderr);
 
-    size_t instruction = vm.ip - vm.chunk->code - 1;
-    int line = vm.chunk->lines[instruction];
-    fprintf(stderr, "[line %d] in script\n", line);
+    // After printing the error message itself, we walk the call stack from top (the most
+    // recently called function) to bottom (the top-level code). For each frame, we find
+    // the line number that corresponds to the current ip inside that frame’s function.
+    // Then we print that line number along with the function name.
+    for (int i = vm.frameCount - 1; i >= 0; i--) {
+        CallFrame* frame = &vm.frames[i];
+        ObjFunction* function = frame->function;
+        // The - 1 is because the IP is already sitting on the next instruction to be executed,
+        // but we want the stack trace to point to the previous failed instruction.
+        size_t instruction = frame->ip - function->chunk.code - 1;
+        fprintf(stderr, "[line %d] in ",
+                function->chunk.lines[instruction]);
+        if (function->name == NULL) {
+            fprintf(stderr, "script\n");
+        } else {
+            fprintf(stderr, "%s()\n", function->name->chars);
+        }
+    }
     resetStack();
+}
+static void defineNative(const char* name, NativeFn function) {
+    // GC: You’re probably wondering why we push and pop the name and function on the stack.
+    // That looks weird, right? This is the kind of stuff you have to worry about when garbage
+    // collection gets involved. Both copyString() and newNative() dynamically allocate memory.
+    // That means once we have a GC, they can potentially trigger a collection. If that happens,
+    // we need to ensure the collector knows we’re not done with the name and ObjFunction so that
+    // it doesn’t free them out from under us. Storing them on the value stack accomplishes that.
+    push(OBJ_VAL(copyString(name, (int)strlen(name))));
+    push(OBJ_VAL(newNative(function)));
+    tableSet(&vm.globals, AS_STRING(vm.stack[0]), vm.stack[1]);
+    pop();
+    pop();
 }
 
 void initVM() {
@@ -44,6 +77,8 @@ void initVM() {
     vm.objects = NULL;
     initTable(&vm.globals);
     initTable(&vm.strings);
+
+    defineNative("clock", clockNative);
 }
 
 void freeVM() {
@@ -67,6 +102,50 @@ Value pop() {
 static Value peek(int distance) {
     return vm.stackTop[-1 - distance];
 }
+static bool call(ObjFunction* function, int argCount) {
+    if (argCount != function->arity) {
+        runtimeError("Expected %d arguments but got %d.",
+            function->arity, argCount);
+        return false;
+    }
+    if (vm.frameCount == FRAMES_MAX) {
+        runtimeError("Stack overflow.");
+        return false;
+    }
+
+    // This simply initializes the next CallFrame on the stack. It stores a pointer to
+    // the function being called and points the frame’s ip to the beginning of the
+    // function’s bytecode. Finally, it sets up the slots pointer to give the frame
+    // its window into the stack. The arithmetic there ensures that the arguments
+    // already on the stack line up with the function’s parameters
+    CallFrame* frame = &vm.frames[vm.frameCount++];
+    frame->function = function;
+    frame->ip = function->chunk.code;
+
+    // The funny little - 1 is to account for stack slot zero which the compiler set
+    // aside for when we add methods later
+    frame->slots = vm.stackTop - argCount - 1;
+    return true;
+}
+static bool callValue(Value callee, int argCount) {
+    if (IS_OBJ(callee)) {
+        switch (OBJ_TYPE(callee)) {
+            case OBJ_FUNCTION:
+                return call(AS_FUNCTION(callee), argCount);
+            case OBJ_NATIVE: {
+                NativeFn native = AS_NATIVE(callee);
+                Value result = native(argCount, vm.stackTop - argCount);
+                vm.stackTop -= argCount + 1;
+                push(result);
+                return true;
+            }
+            default:
+                break; // Non-callable object type.
+        }
+    }
+    runtimeError("Can only call functions and classes.");
+    return false;
+}
 static bool isFalsey(Value value) {
     return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
 }
@@ -85,11 +164,15 @@ static void concatenate() {
 }
 
 static InterpretResult run() {
-#define READ_BYTE() (*vm.ip++)
-#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-// yanks the next two bytes from the chunk and builds a 16-bit unsigned integer out of them
+    // store the current topmost CallFrame in a local variable inside the main bytecode execution function.
+    CallFrame* frame = &vm.frames[vm.frameCount - 1];
+
+#define READ_BYTE() (*frame->ip++)
 #define READ_SHORT() \
-    (vm.ip += 2, (uint16_t)((vm.ip[-2] << 8) | vm.ip[-1]))
+    (frame->ip += 2, \
+    (uint16_t)((frame->ip[-2] << 8) | frame->ip[-1]))
+#define READ_CONSTANT() \
+    (frame->function->chunk.constants.values[READ_BYTE()])
 #define READ_STRING() AS_STRING(READ_CONSTANT())
 #define BINARY_OP(valueType, op) \
     do { \
@@ -111,8 +194,8 @@ static InterpretResult run() {
             printf(" ]");
         }
         printf("\n");
-        disassembleInstruction(vm.chunk,
-                               (int)(vm.ip - vm.chunk->code));
+        disassembleInstruction(&frame->function->chunk,
+            (int)(frame->ip - frame->function->chunk.code));
 #endif
         uint8_t instruction;
         switch (instruction = READ_BYTE()) {
@@ -128,7 +211,8 @@ static InterpretResult run() {
             case OP_POP: pop(); break;
             case OP_GET_LOCAL: {
                 uint8_t slot = READ_BYTE();
-                push(vm.stack[slot]);
+                // accesses the given numbered slot relative to the beginning of that frame
+                push(frame->slots[slot]);
                 break;
             }
             case OP_SET_LOCAL: {
@@ -139,7 +223,7 @@ static InterpretResult run() {
                 // assignment expression is the assigned value itself, so the VM just
                 // leaves the value on the stack.
                 uint8_t slot = READ_BYTE();
-                vm.stack[slot] = peek(0);
+                frame->slots[slot] = peek(0);
                 break;
             }
             case OP_GET_GLOBAL: {
@@ -214,22 +298,44 @@ static InterpretResult run() {
             }
             case OP_JUMP: {
                 uint16_t offset = READ_SHORT();
-                vm.ip += offset;
+                frame->ip += offset;
                 break;
             }
             case OP_JUMP_IF_FALSE: {
                 uint16_t offset = READ_SHORT();
-                if (isFalsey(peek(0))) vm.ip += offset;
+                if (isFalsey(peek(0))) frame->ip += offset;
                 break;
             }
             case OP_LOOP: {
                 uint16_t offset = READ_SHORT();
-                vm.ip -= offset;
+                frame->ip -= offset;
+                break;
+            }
+            case OP_CALL: {
+                // See: https://craftinginterpreters.com/calls-and-functions.html#binding-arguments-to-parameters
+                int argCount = READ_BYTE();
+                if (!callValue(peek(argCount), argCount)) {
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                // If callValue() is successful, there will be a new frame on the CallFrame stack for the called
+                // function. The run() function has its own cached pointer to the current frame, so we need to
+                // update that.
+                frame = &vm.frames[vm.frameCount - 1];
                 break;
             }
             case OP_RETURN: {
-                // Exit interpreter.
-                return INTERPRET_OK;
+                // See: https://craftinginterpreters.com/calls-and-functions.html#returning-from-functions
+                Value result = pop();
+                vm.frameCount--;
+                if (vm.frameCount == 0) {
+                    pop();
+                    return INTERPRET_OK;
+                }
+
+                vm.stackTop = frame->slots;
+                push(result);
+                frame = &vm.frames[vm.frameCount - 1];
+                break;
             }
         }
     }
@@ -242,19 +348,12 @@ static InterpretResult run() {
 }
 
 InterpretResult interpret(const char* source) {
-    Chunk chunk;
-    initChunk(&chunk);
+    ObjFunction* function = compile(source);
+    if (function == NULL) return INTERPRET_COMPILE_ERROR;
 
-    if (!compile(source, &chunk)) {
-        freeChunk(&chunk);
-        return INTERPRET_COMPILE_ERROR;
-    }
-
-    vm.chunk = &chunk;
-    vm.ip = vm.chunk->code;
-
-    InterpretResult result = run();
-
-    freeChunk(&chunk);
-    return result;
+    // Now you can see why the compiler sets aside stack slot zero—that stores the function being called
+    push(OBJ_VAL(function));
+    // set up the first frame for executing the top-level code
+    call(function, 0);
+    return run();
 }
