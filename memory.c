ed7f4581dbc894a088171424a6a696cba08c4cd5
Elizabeth Gilbert <evgilber@andrew.cmu.edu>
2024-10-15 14:24:34 -0400

    bound methods

--- memory.c
+++ memory.c
@@ -15,6 +15,9 @@ static void freeObject(Obj* object) {
 #endif
 
     switch (object->type) {
+        case OBJ_BOUND_METHOD:
+            FREE(ObjBoundMethod, object);
+        break;
         case OBJ_CLASS: {
             ObjClass* klass = (ObjClass*)object;
             freeTable(&klass->methods);
@@ -141,6 +144,12 @@ static void blackenObject(Obj* object) {
     // “black” in the object’s state. A black object is any object whose isMarked field is set and that
     // is no longer in the gray stack.
     switch (object->type) {
+        case OBJ_BOUND_METHOD: {
+            ObjBoundMethod* bound = (ObjBoundMethod*)object;
+            markValue(bound->receiver);
+            markObject((Obj*)bound->method);
+            break;
+        }
         case OBJ_CLASS: {
             ObjClass* klass = (ObjClass*)object;
             markObject((Obj*)klass->name);
